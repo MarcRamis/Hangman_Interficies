bbc8359acde8ee59c7a7ecb2a4a7bf377f590b8b
Marc Ramis <marc.ramis@enti.cat>
2021-12-03 11:13:52 +0100

    Created prefabs

---
+++ Assets/Scripts/Installer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Installer : MonoBehaviour
+{
+    [SerializeField] private RectTransform _canvasParent;
+
+    //[SerializeField] private HomeView _homePanelPrefab;
+    //[SerializeField] private ScoreView _scorePanelPrefab;
+    //[SerializeField] private SettingsView _configPanelPrefab;
+    //[SerializeField] private ButtonsView _barBottomPrefab;
+
+    private void Awake()
+    {
+        
+    }
+}
