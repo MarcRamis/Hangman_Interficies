787f53e364ca887580655a00a425d18f99635d75
Marc Ramis <marc.ramis@enti.cat>
2021-12-07 22:23:35 +0100

    Code organized but i keep using "InvokeRepeat"

---
+++ Assets/Scripts/InitInstaller.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class InitInstaller : MonoBehaviour
+{
+    [SerializeField] private RectTransform _canvasParent;
+
+    [SerializeField] private InitView _initPrefab;
+    FirebaseLogService _firebaseLogService;
+
+    private void Awake()
+    {
+        var initView = Instantiate(_initPrefab, _canvasParent);
+
+        var initViewModel = new InitViewModel();
+        initView.SetViewModel(initViewModel);
+
+        var eventDispatcher = new EventDispatcherService();
+        _firebaseLogService = new FirebaseLogService(eventDispatcher);
+
+        var doAuthUseCase = new DoAuthUseCase(_firebaseLogService, eventDispatcher);
+
+        new InitController(initViewModel);
+        new InitPresenter(initViewModel, eventDispatcher);
+    }
+
+    private void Start()
+    {
+        _firebaseLogService.Init();
+    }
+}
---
+++ Assets/Scripts/Presenters/InitPresenter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InitPresenter : Presenter
+{
+    IEventDispatcherService _eventDispatcher;
+    InitViewModel _viewModel;
+
+    public InitPresenter(InitViewModel viewModel, EventDispatcherService eventDispatcher)
+    {
+        _viewModel = viewModel;
+        _eventDispatcher = eventDispatcher;
+
+        _eventDispatcher.Subscribe<LogEvent>(ChangeScene);
+    }
+
+    private void ChangeScene(LogEvent data)
+    {
+        Debug.Log(data.Text);
+        //SceneManager.LoadScene("Menu");
+    }
+}
